605e6f876a79e9e839f95c9171e06ce4ccfc28aa
15Liona <118630608+15Liona@users.noreply.github.com>
2023-04-01 13:48:52 -0600

    Add files via upload

---
+++ boat.js
@@ -0,0 +1,35 @@
+class Boat {
+   constructor(x, y, w, h, boatpos){
+     var boat_options = {
+        restitution: 0.8,
+        friction: 1.0,
+        density: 1
+     };
+
+     this.body = Bodies.rectangle(x, y, w, h, boat_options);
+     this.width = w;
+     this.height = h;
+     this.image = loadImage("./assets/boat.png");
+     this.boatPosition = boatpos;
+     World.add(world, this.body);
+   }
+
+   remove(index){
+    setTimeout(()=>{
+      Matter.World.remove(world, boats[index].body);
+      delete boats[index];
+    }, 3000);
+  }
+
+   display(){
+     var pos = this.body.position
+     var angle = this.body.angle
+
+     push();
+     translate(pos.x, pos.y);
+     rotate(angle);
+     imageMode(CENTER);
+     image(this.image, 0, this.boatPosition, this.width, this.height);
+     pop();
+   }
+}
